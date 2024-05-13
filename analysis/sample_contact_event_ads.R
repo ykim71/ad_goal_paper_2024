997f629dd40cc3e5cc3976b77aebf9a432f8a4df
Markus Neumann <mneumann.polsci@gmail.com>
2024-05-13 18:37:41 +0800

    sample 10 contact and event ads

---
+++ analysis/sample_contact_event_ads.R
@@ -0,0 +1,22 @@
+library(data.table)
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+bert <- fread("../data/fb2022_predicted_goals_bert_all.csv.gz", data.table = F)
+fb22_text <- fread("../data/fb2022_prepared.csv.gz", data.table = F)
+
+bert <- bert %>% select(ad_id, CONTACT, EVENT)
+fb22_text <- left_join(fb22_text, bert)
+contact_ads <- fb22_text %>% filter(CONTACT == 1)
+event_ads <- fb22_text %>% filter(EVENT == 1)
+
+set.seed(123)
+contact_sample_ids <- sample(1:nrow(contact_ads), 10)
+contact_sample <- contact_ads[contact_sample_ids, c("ad_id", "text")]
+fwrite(contact_sample, "../data/contact_ads_sample.csv")
+
+set.seed(123)
+event_sample_ids <- sample(1:nrow(event_ads), 10)
+event_sample <- event_ads[event_sample_ids, c("ad_id", "text")]
+fwrite(event_sample, "../data/event_ads_sample.csv")
