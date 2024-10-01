bf4eeac33b65b27533cf0342c709a7c0464205af
Markus Neumann <mneumann.polsci@gmail.com>
2024-10-01 17:28:27 +0800

    new bert performance

--- 04_evaluate_distilbert.py
+++ 04_evaluate_distilbert.py
@@ -1,29 +1,30 @@
-import sklearn.model_selection as ms
-from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.calibration import CalibratedClassifierCV
-from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
 import pandas as pd
 import numpy as np
-from joblib import dump, load
 
 results_dir = 'performance'
 
-test = pd.read_csv('data/test.csv', names = ['ad_id', 'text', 'DONATE', 'CONTACT', 'PURCHASE', 'GOTV', 'EVENT', 'POLL', 'GATHERINFO', 'LEARNMORE', "PRIMARY_PERSUADE"])
+test = pd.read_csv('data/test_2020_2022.csv')
 test_pred = pd.read_csv('data/test_predictions_bert.csv')
 
 model_name = 'bert'
 
 goals = ["DONATE", "CONTACT", "PURCHASE", "GOTV", "EVENT", "POLL", "GATHERINFO", "LEARNMORE", "PRIMARY_PERSUADE"]
 
+df_combined_perf = pd.DataFrame(np.nan, index=[0], columns=goals)
+df_combined_perf_weighted_f1 = pd.DataFrame(np.nan, index=[0], columns=goals)
+
 for g in goals:
   
   df_perf = pd.DataFrame(metrics.precision_recall_fscore_support(test[g], test_pred[g]))
+  df_perf['weighted_avg'] = pd.Series(metrics.precision_recall_fscore_support(test[g], test_pred[g], average = "weighted"))
+  df_perf = np.round(df_perf, 3)
   df_perf.index = ['Precision', 'Recall', 'F-Score', 'Support']
   df_perf.to_csv(results_dir + "/" + model_name + "/" + g + '.csv')
   
+  df_combined_perf.at[0,g] = df_perf.iloc[2,1]
+  df_combined_perf_weighted_f1.at[0,g] = df_perf['weighted_avg'][2]
+  
 
+df_combined_perf.to_csv("performance/bert_2020_2022_feature_comparison_class1.csv")
+df_combined_perf_weighted_f1.to_csv("performance/bert_2020_2022_feature_comparison_weighted.csv")
