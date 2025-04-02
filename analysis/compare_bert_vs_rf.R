e8fe008fbe80b43d70a33832ca15c87565fbca18
Markus Neumann <mneumann.polsci@gmail.com>
2025-04-02 17:02:31 +0800

    remove timestamps from performance comparison tables

--- analysis/compare_bert_vs_rf.R
+++ analysis/compare_bert_vs_rf.R
@@ -17,7 +17,7 @@ results <- results[,order(colnames(results))]
 rownames(results) <- c("DistilBERT", "Random Forest")
 
 xt <- xtable(results, digits = 3, label = "model_performance", caption = "DistilBERT and Random Forest classifier performance given as weighted F1 scores. Generally, the DistilBERT classifiers outperformed the Random Forest classifiers reaching a mean F1 score of 0.95 across classifiers.")
-print.xtable(xt, file = "tables/bert_vs_rf_performance.tex")
+print.xtable(xt, file = "tables/bert_vs_rf_performance.tex", comment = F)
 
 mean(as.numeric(results[1,])) # average F1-score reported in the paper
 
@@ -38,7 +38,7 @@ results <- results[,order(colnames(results))]
 rownames(results) <- c("DistilBERT", "Random Forest")
 
 xt <- xtable(results, digits = 3, label = "model_performance_positive", caption = "DistilBERT and Random Forest classifier performance given as F1 scores of the positive cases. Generally, the DistilBERT classifiers outperformed the Random Forest classifiers reaching a mean F1 score of 0.81 across classifiers.")
-print.xtable(xt, file = "tables/bert_vs_rf_performance_positive.tex")
+print.xtable(xt, file = "tables/bert_vs_rf_performance_positive.tex", comment = F)
 
 mean(as.numeric(results[1,])) # average F1-score reported in the paper
 
@@ -56,5 +56,5 @@ results <- results[,order(colnames(results))]
 rownames(results) <-  c("Ad creative body", "Ad creative body + ASR + OCR", "All fields")
 
 xt <- xtable(results, digits = 3, label = "model_performance_feature_comparison", caption = "Random Forest classifier performance for different feature sets, given as weighted F1 scores.")
-print.xtable(xt, file = "tables/rf_performance_features.tex")
+print.xtable(xt, file = "tables/rf_performance_features.tex", comment = F)
 
