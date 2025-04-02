af95c72a65798342a5422309a4c5f258ac55f07f
Markus Neumann <mneumann.polsci@gmail.com>
2025-04-02 16:50:01 +0800

    simplified age script

--- analysis/age.R
+++ analysis/age.R
@@ -9,83 +9,19 @@ library(ggplot2)
 
 
 # Combine relevant vars from multiple datasets
+# Predictions on inference set
 fb22_pred <- fread("../data/fb2022_predicted_goals_bert_all.csv.gz", data.table = F)
-
-goal_names <- data.frame(wrong = c("DONATE", "CONTACT", "PURCHASE", "GOTV", "EVENT", "POLL", "GATHERINFO", "LEARNMORE", "PRIMARY_PERSUADE"),
-                         correct = c("Donate", "Contact", "Purchase", "Vote", "Event", "Poll", "Acquisition", "Learn", "Persuade"))
-rename_strings <- function(x) {
-  ifelse(x %in% goal_names$wrong, goal_names$correct[match(x, goal_names$wrong)], x)
-}
-
-names(fb22_pred) <- rename_strings(names(fb22_pred))
-
-fb22_vars <- fread("../data/fb_2022_adid_var1.csv.gz", data.table = F)
-fb22_vars <- fb22_vars %>% select(ad_id, page_id, pd_id, spend)
-fb22_vars$spend <- str_split_fixed(fb22_vars$spend, ",", 2) %>%
-  apply(., 2, function(x){str_extract(x, "[0-9]+")}) %>%
-  apply(., 1, function(x){mean(as.numeric(x))})
-
-fb22_vars2 <- fread("../../fb_2022/fb_2022_adid_text.csv.gz", data.table = F)
-fb22_vars2 <- fb22_vars2 %>% select(ad_id, page_name)
-
-fb22 <- left_join(fb22_vars, fb22_vars2, by = "ad_id")
+# Main variables
+load("../data/fb_2022_adid_var_clean.rdata")
+fb22 <- fb22_vars %>% select(ad_id, pd_id, spend)
 fb22 <- left_join(fb22, fb22_pred, by = "ad_id")
-
-rm(list = ls()[ls() != "fb22"])
-
-#----
-# Extract age data from the json
-
-extract_age <- function(x){
-  
-  json_str <- str_replace_all(x, "\"\"", "\"")
-  df_demographic <- fromJSON(json_str)
-  df_demographic$percentage <- as.numeric(df_demographic$percentage)
-  df_demographic <- df_demographic %>% group_by(age) %>% summarize(percentage = sum(percentage))
-  
-  age_equivalents <- data.frame(
-    age_group = c("13-17", "18-24", "25-34", "35-44", "45-54", "55-64", "65+"),
-    age = c(15, 21, 29.5, 39.5, 49.5, 59.5, 69.5)
-  )
-  
-  df_demographic$age <- age_equivalents$age[match(df_demographic$age, age_equivalents$age_group)]
-  ad_demographic <- sum(df_demographic$age*df_demographic$percentage)
-  
-  return(ad_demographic)
-}
-
-fb22_vars <- fread("../data/fb_2022_adid_var1.csv.gz", data.table = F)
-test <- fb22_vars[fb22_vars$demographic_distribution != "",]
-
-cl <- makeCluster(12L)
-clusterExport(cl, c("str_replace_all", "fromJSON", "%>%", "summarize", "group_by"))
-ads_age <- pblapply(cl = cl, test$demographic_distribution, extract_age)
-stopCluster(cl)
-
-test$age <- unlist(ads_age)
-age <- test %>% select(ad_id, age)
-
-fb22_age <- right_join(fb22, age, by = "ad_id")
+# Age
+load("../data/ads_age.rdata")
+fb22_age <- right_join(fb22, ads_age, by = "ad_id")
+rm(list = ls()[ls() != "fb22_age"])
 
 #----
-# Make the plot(s)
-
-# These aren't actually used in the paper
-ggplot(fb22_age, aes(age)) + geom_density()
-ggplot(fb22_age[fb22_age$Donate == 1,], aes(age)) + geom_density()
-ggplot(fb22_age[fb22_age$Contact == 1,], aes(age)) + geom_density()
-ggplot(fb22_age[fb22_age$Purchase == 1,], aes(age)) + geom_density()
-ggplot(fb22_age[fb22_age$Vote == 1,], aes(age)) + geom_density()
-ggplot(fb22_age[fb22_age$Event == 1,], aes(age)) + geom_density()
-ggplot(fb22_age[fb22_age$Poll == 1,], aes(age)) + geom_density()
-ggplot(fb22_age[fb22_age$Info == 1,], aes(age)) + geom_density()
-ggplot(fb22_age[fb22_age$Learn == 1,], aes(age)) + geom_density()
-ggplot(fb22_age[fb22_age$Persuade == 1,], aes(age)) + geom_density()
-
-mean(fb22_age$age[fb22_age$Donate == 1], na.rm = T)
-mean(fb22_age$age[fb22_age$Vote == 1], na.rm = T)
-mean(fb22_age$age[fb22_age$Learn == 1], na.rm = T)
-
+# Make the plot
 fb22_age_df <- fb22_age %>% select(Donate:Persuade) %>% as.matrix(.) * fb22_age$age
 fb22_age_df <- as.data.frame(fb22_age_df)
 fb22_age_df$ad_id <- fb22_age$ad_id
@@ -102,4 +38,3 @@ ggplot(fb22_age_df, aes(Age)) +
   theme_bw() +
   labs(y = "")
 ggsave("figures/age_by_goal.pdf", width = 5, height = 3.5)
-
