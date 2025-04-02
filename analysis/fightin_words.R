55da08e37f5e02cde814ec62c94ca5910fe73897
Markus Neumann <mneumann.polsci@gmail.com>
2025-04-02 19:32:01 +0800

    update fightin words script (results are the same)

--- analysis/fightin_words.R
+++ analysis/fightin_words.R
@@ -9,12 +9,6 @@ library(slam)
 library(xtable)
 
 fb22 <- fread("../data/fb2022_predicted_goals_bert_all.csv.gz", data.table = F)
-goal_names <- data.frame(wrong = c("DONATE", "CONTACT", "PURCHASE", "GOTV", "EVENT", "POLL", "GATHERINFO", "LEARNMORE", "PRIMARY_PERSUADE"),
-                         correct = c("Donate", "Contact", "Purchase", "Vote", "Event", "Poll", "Info", "Learn", "Persuade"))
-rename_strings <- function(x) {
-  ifelse(x %in% goal_names$wrong, goal_names$correct[match(x, goal_names$wrong)], x)
-}
-names(fb22) <- rename_strings(names(fb22))
 fb22$Nogoals <- fb22 %>% select(-ad_id) %>% apply(., 1, function(x){all(x == 0)}) %>% as.numeric()
 fb22_text <- fread("../data/fb2022_inference.csv.gz", data.table = F)
 
@@ -33,7 +27,7 @@ docvars(tks, "Purchase") <- fb22$Purchase
 docvars(tks, "Vote") <- fb22$Vote
 docvars(tks, "Event") <- fb22$Event
 docvars(tks, "Poll") <- fb22$Poll
-docvars(tks, "Info") <- fb22$Info
+docvars(tks, "Acquisition") <- fb22$Acquisition
 docvars(tks, "Learn") <- fb22$Learn
 docvars(tks, "Persuade") <- fb22$Persuade
 fb22$nogoals <- fb22 %>% select(ends_with("_prediction")) %>% apply(., 1, function(x){all(x == 0)}) %>% as.numeric()
@@ -55,7 +49,7 @@ goals <- c("Donate",
            "Vote",
            "Event",
            "Poll",
-           "Info",
+           "Acquisition",
            "Learn",
            "Persuade",
            "No goals")
@@ -72,10 +66,8 @@ for(i in 1:length(goals)){
   dfg[goals[i]] <- fs[['1']]$term[1:20]
 }
 
-dfg %>% rename(Acquisition = "Info")
-
 # rename and reorder columns
-dfg <- dfg %>% rename(Acquisition = "Info") %>% select("Acquisition", "Contact", "Donate", "Event", "Learn", "Persuade", "Poll", "Purchase", "Vote", "No goals")
+dfg <- dfg %>% select("Acquisition", "Contact", "Donate", "Event", "Learn", "Persuade", "Poll", "Purchase", "Vote", "No goals")
 # We don't actually discuss the No goals category, so omit
 dfg <- dfg %>% select(-`No goals`)
 
@@ -85,4 +77,5 @@ print(xtable(dfg,
              label = "tab:fightin_words"),
       "latex",
       "tables/fightin_words.tex",
-      include.rownames = F)
+      include.rownames = F,
+      comment = F)
